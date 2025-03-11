19317bc3cb4e89beedd844e9b122bf7bc2c832ac
ethanrolbiecki <ethan.rolbiecki10@gmail.com>
2025-03-11 17:16:07 +0000

    Tue, Mar 11, 2025, 10:16 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -1,1 +1,11 @@
-c
+const sounds = ['applause','boo','gasp','tada',
+    'victory','wrong']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerHTLM = sound
+
+
+    document.getElementById('buttons').appendChild(btn)
+})
