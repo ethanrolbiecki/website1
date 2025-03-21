b9d9ad26160332441d0a9016c5ae9f3e57e20f00
ethanrolbiecki <ethan.rolbiecki10@gmail.com>
2025-03-21 16:53:06 +0000

    Fri, Mar 21, 2025, 9:53 AM -07:00

---
+++ js/insect.js
@@ -0,0 +1,59 @@
+const screens = document.querySelectorAll('.screen')
+const start_btn = document.getElementById('start-btn')
+const choose_insects_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+
+let seconds = 0
+let score = 0
+let selected_insect = {}
+start_btn.addEventListener('click', ()=> {
+    screens[0].classList.add('up')
+})
+
+choose_insects_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        screens[1].classList.add('up')
+        const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAtrribute('alt')
+        selected_insect = {src, alt}
+        StartGame()
+        createInsect()
+    })
+})
+
+
+function startGame(){
+    setInterval(increaseTime, 1000)
+}
+
+function increaseTime() {
+    let s = seconds % 60;
+    let m = Math.floor(seconds / 60)
+    if (s < 10){
+        s = `0${s}`
+    }
+    if (m < 10){
+        m = `0${m}`
+    }
+    timeEl.innerHTML = `Time: ${m}:${s}`
+    seconds = seconds + 1;
+
+}
+
+function createInsect(){
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    insect.innerHTML = `<img src = "${selected_insect.src}" alt = "${selected_insect.alt}>`
+
+    game_container.appendChild(insect)
+}
+
+function getRandomLocation(){
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const x = Math.random() *
+}
