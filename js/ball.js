c9f154681c88e010c1e000ba4a27b5b1a6ed2b19
ethanrolbiecki <ethan.rolbiecki10@gmail.com>
2025-04-04 16:48:28 +0000

    Fri, Apr 4, 2025, 9:48 AM -07:00

---
+++ js/ball.js
@@ -0,0 +1,9 @@
+const ball = document.createElement('div')
+
+function createBall(){
+    document.body.appendChild(ball)
+    ball.style.height = "100px"
+    ball.style.width = "100px"
+    ball.style.borderRadius = "50%"
+    ball.style.backgroundColor = "green"
+}
