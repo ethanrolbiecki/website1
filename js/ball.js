927a47a9ac47fb981ae6f169377756d881e62ce5
ethanrolbiecki <ethan.rolbiecki10@gmail.com>
2025-04-04 16:58:10 +0000

    Fri, Apr 4, 2025, 9:58 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -10,8 +10,8 @@ function createBall(){
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "green"
     ball.style.position = "absolute"
-    ball.style.top = `${windowHeight/2}px`
-    ball.style.left = `${windowWidth/2}px`
+    ball.style.top = `${windowHeight/2 - ballRadius}px`
+    ball.style.left = `${windowWidth/2 - ballRadius}px`
 }
 
 createBall()
