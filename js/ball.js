8ba1594abd63f0131e54b457cf035e460220122f
ethanrolbiecki <ethan.rolbiecki10@gmail.com>
2025-04-04 17:03:55 +0000

    Fri, Apr 4, 2025, 10:03 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -2,10 +2,13 @@ const ball = document.createElement('div')
 const ballRadius = 30
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
+let ballXPosition = windowWidth/2 - ballRadius
 
 setInvertalval(moveBall, 10)
+
 function moveBall() {
-    ball.style.left =
+    ball.style.left = ballXPosition+5
+    ball.style.left = `${ballXPosition}px`
 }
 createBall()
 
